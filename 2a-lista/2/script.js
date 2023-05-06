a73c44e9d394987ca95f48e6d7ba39f2de8ebb4b
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2023-05-06 20:03:56 -0300

    2a-lista: Redesenha solução da 2a questão

--- 2a-lista/2/script.js
+++ 2a-lista/2/script.js
@@ -1,4 +1,6 @@
-const num1 = parseInt(prompt("Número 1: "))
-const num2 = parseInt(prompt("Número 2: "))
-const soma = num1 + num2
-window.alert(`${num1} + ${num2} = ${soma}`)
+const num1 = parseInt(prompt("Número 1: "));
+const num2 = parseInt(prompt("Número 2: "));
+const soma = num1 + num2;
+const s_soma = `${num1} + ${num2} = ${soma}`;
+window.alert(s_soma);
+document.write(s_soma);
