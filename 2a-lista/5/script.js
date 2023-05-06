f9aa4026bddb3769ecdbbbf8b5024646e1efd145
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2023-05-06 20:12:42 -0300

    2a-lista: Refatora solução da 5a questão

--- 2a-lista/5/script.js
+++ 2a-lista/5/script.js
@@ -1,7 +1,7 @@
-"use strict"
-
-const linhas = parseInt(prompt("Altura da pirâmide: "));
+const linhas = parseInt(prompt("Altura da pirâmide"));
+// Controla linhas
 for (let i = linhas; i > 0; i--) {
+    // Controla colunas
     for (let j = i; j > 0; j--) {
         if (i >= 10) {
             document.writeln(i);
