e0163f25271956237b974e278a0384beed79255e
cc23521 <cc23521@g.unicamp.br>
2023-05-11 21:50:01 -0300

    2a-lista: Adiciona solução final da 7a questão

--- 2a-lista/7/script.js
+++ 2a-lista/7/script.js
@@ -1,7 +1,33 @@
-let textoVisor = document.getElementById("visor").value;
+let textoVisor = document.getElementById("visor");
+
+function clear() {
+    textoVisor.value = "";
+}
+
+function criptografia1() {
+    textoVisor.value = "101010";
+}
+
+function criptografia2() {
+    textoVisor.value = "42";
+}
 
 document.querySelectorAll(".botao").forEach(botao => {
     botao.addEventListener("click", e => {
-        document.getElementById("visor").value = document.getElementById("visor").value + botao.value;
+        if (botao.value === "CE") {
+            clear();
+        }
+        else if (botao.value === "CR 1") {
+            criptografia1();
+        }
+        else if (botao.value === "CR 2") {
+            criptografia2();
+        }
+        else {
+            if (textoVisor.value === "101010" || textoVisor.value === "42") {
+                clear();
+            }
+            textoVisor.value = textoVisor.value + botao.value;
+        }     
     })
-})
+}) 
